4651cdd1895c02446e0ef68d7b69d5bbdbf7d4ab
syagu <sketdance060@yahoo.co.jp>
2020-02-20 09:51:37 +1300

    StepAsideUnityChanのプログラム
    カメラ外に出ると、オブジェクトが消えるようにした。

---
+++ Assets/DestroyObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DestroyObject : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject maincamera;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        maincamera = Camera.main.gameObject;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.z < maincamera.transform.position.z)
+        {
+            
+            Destroy(this.gameObject);
+        }
+    }
+}
